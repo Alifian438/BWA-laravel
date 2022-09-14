e62dd0e1692831a09fe091a0925dbd8400cdbeb0
alifian438 <alifian290126@gmai.com>
2022-09-14 07:34:43 +0700

    menambahkan seeders dan foldering pada controller dan view

---
+++ database/seeders/ConfigPaymentSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\MasterData\ConfigPayment;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ConfigPaymentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     */
+    // * @return void
+    public function run()
+    {
+        $config_payment = [
+            [
+                'fee' => '150000',
+                'vat' => '20',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ], 
+        ]; 
+
+        //untuk insert ke table specialist
+        ConfigPayment::insert($config_payment);
+    }
+}
